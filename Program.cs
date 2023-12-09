4e7ff8d0a66505f5eb9290be0dd719dc00f42abe
Emre Küçük <emrekucuk74@hotmail.com>
2023-12-09 23:44:08 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using SautinSoft;
+
+// Read Example File
+var filePath = Path.Combine(Directory.GetCurrentDirectory(), "example.doc");
+var reader = new StreamReader(filePath);
+var content = await reader.ReadToEndAsync();
+
+// Replace Content
+content = content.Replace("{{NAME}}", "II.");
+content = content.Replace("{{SURNAME}}", "Kleopat");
+
+System.Console.WriteLine(content);
+
+// New Example File
+var newFilePath = "new-example.doc";
+var newFile = File.Create(newFilePath);
+var streamWriter = new StreamWriter(newFile, System.Text.Encoding.UTF8);
+await streamWriter.WriteAsync(content);
+await streamWriter.FlushAsync();
+streamWriter.Close();
+
+
+// Convert to Html
+string htmlFilePath = Path.GetFullPath("example.html");
+
+RtfToHtml rtf = new RtfToHtml();
+rtf.Convert(newFilePath, htmlFilePath);
+
+
+// Convert to Pdf
+
+string pdfFile = @"example.pdf";
+
+SautinSoft.PdfMetamorphosis p = new SautinSoft.PdfMetamorphosis();
+
+p.HtmlToPdfConvertFile(htmlFilePath, pdfFile);
---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Readme
+
+## Convert Document to Html, Html to Pdf
+
+### Steps
+
+- Read to word file
+- Replace to content
+- Write to new file
+- Convert Word to Html
+- Convert Html to Pdf
+
+## Nuget Packages
+
+### SautinSoft
